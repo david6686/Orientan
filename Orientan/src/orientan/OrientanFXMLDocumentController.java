dfb0868073a832014e94b444a829b554164726f8
david6686 <alucard6686@gmail.com>
2016-11-13 20:43:01 +0800

    try to use source code

--- Orientan/src/orientan/OrientanFXMLDocumentController.java
+++ Orientan/src/orientan/OrientanFXMLDocumentController.java
@@ -11,25 +11,28 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import ImageSetChooser.*;
 
 /**
  *
  * @author user
  */
 public class OrientanFXMLDocumentController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+        ImageSetChooser chooserlayout =new ImageSetChooser();
+        chooserlayout.ImageSetChooser();
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
